9bbd03e06709835a925e07e0020b28429998173f
David Valachovic <David.E.Valachovic@gmail.com>
2018-11-04 11:19:41 -0600

    fixed next tick and back tick input stuff

--- Assets/Scripts/DG_Input.cs
+++ Assets/Scripts/DG_Input.cs
@@ -7,12 +7,12 @@ public static class DG_Input {
 	public static bool GoRight() => Input.GetKeyDown(KeyCode.D);
 	public static bool GoDown() => Input.GetKeyDown(KeyCode.S);
 	public static bool GoLeft() => Input.GetKeyDown(KeyCode.A);
-	
+
 	public static bool ToggleManualTick() => Input.GetKeyDown(KeyCode.P);
 	public static bool NextTick() => Input.GetKeyDown(KeyCode.N);
 	public static bool BackTick() => Input.GetKeyDown(KeyCode.B);
 	public static bool NextTickBig() => NextTick() && Shift();
 	public static bool BackTickBig() => BackTick() && Shift();
 
-	static bool Shift() => Input.GetKeyDown(KeyCode.LeftShift) && Input.GetKeyDown(KeyCode.RightShift);
+	static bool Shift() => Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift);
 }
