3b45a62db013403f83caafe843abfb67bb49b7bb
David Valachovic <David.E.Valachovic@gmail.com>
2018-11-11 20:02:48 -0600

    added quit button to online menu

--- Assets/Scripts/StateMachine/GameOnlineMenu.cs
+++ Assets/Scripts/StateMachine/GameOnlineMenu.cs
@@ -5,18 +5,24 @@ using UnityEngine.Networking;
 
 public class GameOnlineMenu : GameStateMachine {
 	public NetworkManagerHUD NetworkManagerHUD;
+	public bool quitButton { get; set; }
 
 	public override void Enter() {
 		NetworkManagerHUD.enabled = true;
+		quitButton = false;
+		GameUI.I.ToggleInGameMenu(true);
 	}
 
 	public override void Exit() {
 		NetworkManagerHUD.enabled = false;
+		GameUI.I.ToggleInGameMenu(false);
 	}
 
 	public override GameStateMachine GetNextState() {
 		if (NetworkManager.singleton.client != null && NetworkManager.singleton.client.isConnected) {
 			return GetComponent<GameJoining>();
+		} else if (quitButton) {
+			return GetComponent<GameMenu>();
 		} else {
 			return null;
 		}
